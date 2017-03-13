ab7b7735382b5029f0e11f70ae1e653b0d13a254
Kevin Berry <kberry@geoforce.com>
2017-03-13 18:22:01 +0000

    ewps

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,4 @@ license          'MIT'
 description      'Simple vhost redirector bolt-on for the nginx cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
-requires "nginx"
-
+depends "nginx"
